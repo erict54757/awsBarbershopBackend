125d458edce0e205a033b5c5020dc5062c82c29a
eric taft <erict54757@gmail.com>
2020-01-17 12:46:24 -0500

    change client route

--- server.js
+++ server.js
@@ -112,7 +112,8 @@ app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.send("hello")
+  res.status(404).send("your on the right track")
 });
 
 
