01b1f567496aac23141dfcf705c3c938580cf8f1
eric taft <erict54757@gmail.com>
2020-01-17 12:28:14 -0500

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const PORT = 4433;
 
 const app = express();
 const http = require("http");
